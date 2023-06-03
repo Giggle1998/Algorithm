2aa1a0213d5d2d2edd7fa0ee9a857214fbd9a183
Giggle1998 <rlvy98@naver.com>
2023-06-04 00:38:27 +0900

    0604

---
+++ baekjoon/11286.py
@@ -0,0 +1,14 @@
+import sys, heapq
+input = sys.stdin.readline
+
+N = int(input())
+heap = []
+for _ in range(N):
+    tmp = int(input())
+    if tmp == 0:
+        if len(heap):
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, [abs(tmp), tmp])

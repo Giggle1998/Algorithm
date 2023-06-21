17f6341260fa2b008f2392f9f52ae0d7631adcd5
Giggle1998 <rlvy98@naver.com>
2023-06-21 21:57:42 +0900

    0621

--- baekjoon/17413.py
+++ baekjoon/17413.py
@@ -1,36 +1,35 @@
-import re
-sent = input()
+'''
+스택을 활용하여 문제를 해결했습니다.
+'''
+data = input()
 
-# sent1 = sent.split('')
-# print(sent1)
+stack = []
+ans = ''
+check = False # 괄호안의 여부를 체크
+for d in data:
+    # 스택에 존재하는 값을 역으로 추가합니다.
+    if d == '<':
+        check = True
+        for _ in range(len(stack)):
+            ans += stack.pop()
 
-ho_1 = 0
-ho_2 = 0
-if sent[0] == '<':
-    for i in range(len(sent)):
-        if sent[i] == '>':
-            ho_1 = i
-            break
+    stack.append(d)
+    # 스택에 존재하는 값은 괄호안의 값이기에 순차적으로 추가합니다.
+    if d == '>':
+        check = False
+        for _ in range(len(stack)):
+            ans += stack.pop(0)
+    # 스택에 존재하는 값을 역으로 추가합니다.
+    if d == ' ' and check == False:
+        for i in range(len(stack)):
+            if i == 0:
+                stack.pop()
+                continue
+            ans += stack.pop()
+        ans += ' '
+# 스택에 값이 남아있는 경우는 괄호의 경우가 아니기에 역으로 추가합니다.
+if stack:
+    for _ in range(len(stack)):
+        ans += stack.pop()
 
-    for j in range(ho_1+1, len(sent)):
-        if sent[j] == '<':
-            ho_2 = j
-            break
-    #### 태그 안에 단어 처리
-    ans = sent[ho_1 + 1:ho_2]
-    ans = ans.split()
-    print(sent[:ho_1 + 1],end="")
-    for a in ans:
-        print(a[::-1],end=" ")
-    print(sent[ho_2:])
-
-else:
-    sent = sent.split()
-    for i in range(len(sent)):
-        print(sent[i][::-1], end=" ")
-
-
-
-
-
-
+print(ans)

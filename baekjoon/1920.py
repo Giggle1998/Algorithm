3e8c8068954934e9708b093883820a21ed7949ce
Giggle1998 <rlvy98@naver.com>
2023-06-26 23:03:03 +0900

    0626

--- baekjoon/1920.py
+++ baekjoon/1920.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+
 N = int(input())
 N_li = sorted(map(int, input().split()))
 M = int(input())
@@ -11,7 +12,7 @@ M_li = list(map(int, input().split()))
 #     else:
 #         print(0)
 
-for num in M_li: # 시간복잡도 logN -> 이분탐색
+for num in M_li: # 시간복잡도 logN -> 이분탐색을 통해 문제 해결
     ans = 0
     start, end = 0, len(N_li) - 1
 
@@ -24,4 +25,5 @@ for num in M_li: # 시간복잡도 logN -> 이분탐색
         else:
             ans = 1
             break
+
     print(ans)

040f62f92756e0b224201675b9c07deea4e34c1b
Giggle1998 <rlvy98@naver.com>
2023-07-03 23:42:06 +0900

    0703

---
+++ programmers/K번째수.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class K번째수 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < commands.length; i++) {
+            for (int j = commands[i][0]; j <= commands[i][1]; j++) {
+                list.add(array[j-1]);
+                Collections.sort(list); // ArrayList 오름차순 정렬
+            }
+            answer[i] = list.get(commands[i][2] - 1);
+            list.clear();
+        }
+        return answer;
+    }
+}

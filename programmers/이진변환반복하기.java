d1bb55275e2658a225f417e3b5163d34162045be
KwonMinWoo <rlvy98@naver.com>
2023-08-02 23:04:15 +0900

    0802

--- programmers/이진변환반복하기.java
+++ programmers/이진변환반복하기.java
@@ -1,23 +1,28 @@
 import java.util.*;
 class 이진변환반복하기 {
-    static boolean[] check;
-    static int cnt = -1;
-    public int solution(int k, int[][] dungeons) {
-        check = new boolean[dungeons.length];
-        dfs(dungeons, 0, k);
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        String target = s;
+        int rst = s.length();
 
-        return cnt;
-    }
-    static void dfs(int[][] dungeons, int n, int k) {
-
-        for (int i=0; i<dungeons.length; i++) {
-            // 아직 방문을 안했거나 탐색 조건에 만족하면 dfs 탐색
-            if (check[i] == false && k >= dungeons[i][0]) {
-                check[i] = true;
-                dfs(dungeons, n+1, k-dungeons[i][1]);
-                check[i] = false;
+        while (true) {
+            int cnt = 0;
+            for(int i=0; i<target.length(); i++) {
+                if (target.charAt(i)=='0') {
+                    cnt++; // 0 count
+                }
             }
+            answer[0]++;
+            answer[1] += cnt;
+            rst -= cnt;
+
+            if (rst==1) {
+                break;
+            }
+
+            target = Integer.toBinaryString(rst);
+            rst = target.length();
         }
-        cnt = Math.max(cnt, n);
+        return answer;
     }
 }

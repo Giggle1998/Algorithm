1468e260c21e8c747ed199aa511528f2d9e037fb
Giggle1998 <rlvy98@naver.com>
2023-08-02 23:08:12 +0900

    0802

--- programmers/이진변환반복하기.java
+++ programmers/이진변환반복하기.java
@@ -4,7 +4,7 @@ class 이진변환반복하기 {
         int[] answer = new int[2];
         String target = s;
         int rst = s.length();
-
+        // 해당 반복문을 탐색
         while (true) {
             int cnt = 0;
             for(int i=0; i<target.length(); i++) {
@@ -19,7 +19,7 @@ class 이진변환반복하기 {
             if (rst==1) {
                 break;
             }
-
+            // 다시 이진수로 변환
             target = Integer.toBinaryString(rst);
             rst = target.length();
         }

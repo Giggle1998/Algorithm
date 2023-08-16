10ef5f03d7e89bbf8c023277073a339edc3f048e
Giggle1998 <rlvy98@naver.com>
2023-08-16 23:53:55 +0900

    0816

---
+++ programmers/땅따먹기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class 땅따먹기 {
+    int solution(int[][] land) {
+        int answer = 0;
+
+        for (int i = 1; i<land.length; i++) {
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][1]));
+        }
+        // System.out.println(Arrays.deepToString(land));
+        for (int num : land[land.length - 1]) {
+            answer = Math.max(answer, num);
+        }
+        return answer;
+    }
+}

82265b935117cd967520ead0078ce362a1d5011c
Giggle1998 <rlvy98@naver.com>
2023-08-20 22:42:41 +0900

    0820

---
+++ programmers/배열자르기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class 배열자르기 {
+    public int solution(int[] number) {
+        int answer = 0;
+
+        for (int i = 0; i<number.length; i++) {
+            int tmp = number[i];
+            for (int j = i+1; j<number.length; j++) {
+                tmp += number[j];
+                for (int z = j + 1; z<number.length; z++) {
+                    if (tmp + number[z] == 0) {
+                        answer++;
+                        // break;
+                    }
+                }
+                tmp -= number[j];
+            }
+        }
+        return answer;
+    }
+}

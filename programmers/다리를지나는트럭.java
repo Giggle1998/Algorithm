b4f404556964fbcf6fc8c4b3b588e75b2d8f62bd
Giggle1998 <rlvy98@naver.com>
2023-09-06 23:54:13 +0900

    0906

---
+++ programmers/다리를지나는트럭.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class 다리를지나는트럭 {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+
+        Queue<Integer> rst = new LinkedList<Integer>();
+        Queue<Integer> bridge = new LinkedList<Integer>();
+
+        for (int truck : truck_weights) {
+            rst.add(truck);
+        }
+
+        while(!rst.isEmpty()) {
+        }
+        return answer;
+    }
+}

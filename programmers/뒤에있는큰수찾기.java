4f0aa7fa27004c55bbb0a7f3ef36636f86a95225
Giggle1998 <rlvy98@naver.com>
2023-10-07 19:17:01 +0900

    1007

--- programmers/뒤에있는큰수찾기.java
+++ programmers/뒤에있는큰수찾기.java
@@ -1,31 +1,27 @@
-import java.util.Stack;
+import java.util.*;
 
-public class 뒤에있는큰수찾기 {
+class Solution {
     public int[] solution(int[] numbers) {
-        Stack<Integer> stack = new Stack<>();
-
-        // 정답 배열 생성
-        int[] arr = new int[numbers.length];
-
-        // 첫 번째 number 인덱스 stack에 push
-        stack.push(0);
-
-        // 두 번째 원소부터 numbers 탐색
-        for (int i = 1; i < numbers.length; i++) {
-            // 스택이 비어있지 않으면서 현재 스택이 바라보고 있는 값보다 number의 값이 크면 뒤에 있는 큰 수 해당
-            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
-                // 뒤에 있는 큰 수에 해당하는 모든 값 pop
-                arr[stack.pop()] = numbers[i];
+        int[] answer = new int[numbers.length];
+        // 2중 for문은 시간초과로 불가능
+        Stack<Integer> stk = new Stack<Integer>();
+        
+        answer[numbers.length - 1] = -1;
+        int idx = 1;
+        stk.push(0);
+        for (int i = 1; i<numbers.length; i++) {
+            
+            while (!stk.isEmpty() && numbers[stk.peek()] < numbers[i]) {
+                answer[stk.pop()] = numbers[i];
             }
-            // 현재 인덱스 push
-            stack.push(i);
+            stk.push(i);
         }
-        // 모든 index를 탐색 후 뒤에 있는 큰 수가 없는 경우 -1 
-        while (!stack.isEmpty()) {
-            arr[stack.pop()] = -1;
+        // System.out.println(Arrays.toString(answer));
+        // System.out.println(stk);
+        while (!stk.isEmpty()) {
+            answer[stk.pop()] = -1;
         }
         
-        // 정답 반환
-        return arr;
+        return answer;
     }
 }

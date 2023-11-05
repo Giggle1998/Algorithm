9c40a55dbcaf209edfe90196ae9ebd51462ab1e2
Giggle1998 <rlvy98@naver.com>
2023-11-05 23:53:59 +0900

    1105

--- baekjoon/BOJ_9251.java
+++ baekjoon/BOJ_9251.java
@@ -14,8 +14,10 @@ public class BOJ_9251 {
 
 		for (int i = 1; i<=str1.length; i++) {
 			for (int j = 1; j<=str2.length; j++) {
+				// 문자가 동일한 경우 -> 이전값에 +1
 				if (str1[i-1] == str2[j-1]) {
 					dp[i][j] = dp[i-1][j-1] + 1;
+				// 문자가 동일하지 않은 경우 -> 최대값 탐색
 				} else {
 					dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
 				}

270314957151ee1f5615dadcc3eaccc8667e2671
Giggle1998 <rlvy98@naver.com>
2023-11-09 23:03:13 +0900

    1109

--- programmers/줄서는방법.java
+++ programmers/줄서는방법.java
@@ -1,52 +1,24 @@
 import java.util.*;
 
 class 줄서는방법 {
-    static int[] arr;
-    static boolean[] visited;
-    static int[] answer;
-    static List<int[]> output = new ArrayList<>();
-    static int arrSize;
-    
-    public void permutation(int depth) {
-        if (depth == arrSize) { // 3개 모두 뽑았을 경우
-            // System.out.println("answer: " + Arrays.toString(answer));
-
-            int[] dupArr = new int[arrSize];
-            for (int i = 0; i < arrSize; i++) {
-                dupArr[i] = answer[i];
-            }
-
-            output.add(dupArr);
-
-            return;
-        }
-
-        for (int i = 0; i < arrSize; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-
-                answer[depth] = arr[i];
-                permutation(depth + 1);
-
-                visited[i] = false;
-            }
-        }
-
-    }
-    
     public int[] solution(int n, long k) {
-        arr = new int[n];
-        visited = new boolean[n];
-        answer = new int[n];
-        arrSize = n;
+        int[] answer = new int[n];
+        List<Integer> list = new ArrayList<>();
 
-        for (int i = 1; i <= n; i++) {
-            arr[i - 1] = i;
+        long f = 1;
+        for(int i=1; i<=n; i++) {
+            list.add(i);
+            f *= i;
         }
 
-        permutation(0);
+        k--; 
+        int idx = 0;
+        while(idx < n) {
+            f /= n - idx;
+            answer[idx++] = list.remove((int) (k / f));
+            k %= f;
+        }
 
-
-        return output.get(Integer.parseInt(String.valueOf(k - 1)));
+        return answer;
     }
 }
